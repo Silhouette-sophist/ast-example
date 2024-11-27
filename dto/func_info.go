486787f7fc2d2d4b56449a35085094e933bfff1c
Silhouette <54932288+Silhouette-sophist@users.noreply.github.com>
2024-11-27 23:45:00 +0800

    [Optimize]添加采集函数参数和返回值信息

--- dto/func_info.go
+++ dto/func_info.go
@@ -2,10 +2,17 @@ package dto
 
 // FunctionInfo 结构体，用于存储函数信息
 type FunctionInfo struct {
-	AFile     string
-	RFile     string
-	Name      string
-	StartLine int
-	EndLine   int
-	Hash      string
+	AFile       string
+	RFile       string
+	Name        string
+	StartLine   int
+	EndLine     int
+	Hash        string
+	Params      []ParamInfo
+	ReturnTypes []string
+}
+
+type ParamInfo struct {
+	Name string
+	Type string
 }
--- visitor/common_func_visitor.go
+++ visitor/common_func_visitor.go
@@ -4,6 +4,7 @@ import (
 	"ast-example/dto"
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
 	"go/ast"
 	"go/printer"
 	"go/token"
@@ -30,14 +31,36 @@ func (v *CommonFuncVisitor) Visit(node ast.Node) ast.Visitor {
 		endPos := v.Fset.Position(fn.End())
 		// 生成函数体的哈希
 		hash := generateBaseFuncHash(fn)
+		// 提取返回值信息
+		var returnTypes []string
+		if fn.Type.Results != nil {
+			for _, result := range fn.Type.Results.List {
+				returnTypes = append(returnTypes, formatType(result.Type))
+			}
+		}
+		// 提取参数信息
+		var params []dto.ParamInfo
+		if fn.Type.Params != nil {
+			for _, param := range fn.Type.Params.List {
+				paramType := formatType(param.Type)
+				for _, name := range param.Names {
+					params = append(params, dto.ParamInfo{
+						Name: name.Name,
+						Type: paramType,
+					})
+				}
+			}
+		}
 		// 将函数信息添加到列表
 		v.Functions = append(v.Functions, &dto.FunctionInfo{
-			AFile:     v.AFile,
-			RFile:     v.RFile,
-			Name:      funcName,
-			StartLine: startPos.Line,
-			EndLine:   endPos.Line,
-			Hash:      hash,
+			AFile:       v.AFile,
+			RFile:       v.RFile,
+			Name:        funcName,
+			StartLine:   startPos.Line,
+			EndLine:     endPos.Line,
+			Hash:        hash,
+			ReturnTypes: returnTypes,
+			Params:      params,
 		})
 	case *ast.FuncLit:
 		// 提取匿名函数的信息
@@ -45,14 +68,36 @@ func (v *CommonFuncVisitor) Visit(node ast.Node) ast.Visitor {
 		endPos := v.Fset.Position(fn.End())
 		// 生成函数体的哈希
 		hash := generateAnonymousFuncHash(fn)
+		// 提取返回值信息
+		var returnTypes []string
+		if fn.Type.Results != nil {
+			for _, result := range fn.Type.Results.List {
+				returnTypes = append(returnTypes, formatType(result.Type))
+			}
+		}
+		// 提取参数信息
+		var params []dto.ParamInfo
+		if fn.Type.Params != nil {
+			for _, param := range fn.Type.Params.List {
+				paramType := formatType(param.Type)
+				for _, name := range param.Names {
+					params = append(params, dto.ParamInfo{
+						Name: name.Name,
+						Type: paramType,
+					})
+				}
+			}
+		}
 		// 在匿名函数中没有名称，使用特定字符串表示
 		v.Functions = append(v.Functions, &dto.FunctionInfo{
-			AFile:     v.AFile,
-			RFile:     v.RFile,
-			Name:      "anonymous function", // 使用占位符
-			StartLine: startPos.Line,
-			EndLine:   endPos.Line,
-			Hash:      hash,
+			AFile:       v.AFile,
+			RFile:       v.RFile,
+			Name:        "anonymous function", // 使用占位符
+			StartLine:   startPos.Line,
+			EndLine:     endPos.Line,
+			Hash:        hash,
+			ReturnTypes: returnTypes,
+			Params:      params,
 		})
 	}
 	return v
@@ -69,4 +114,10 @@ func generateAnonymousFuncHash(fn *ast.FuncLit) string {
 	h := sha256.New()
 	h.Write([]byte(src.String()))
 	return hex.EncodeToString(h.Sum(nil))
+}
+
+// formatType 用于格式化返回值类型
+func formatType(t ast.Expr) string {
+	// 这里可以根据类型表达式进行格式化
+	return fmt.Sprintf("%s", t) // 你可以根据需要返回更详细的类型信息
 }
